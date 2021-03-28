43ee8265f0ce3a4f4969f4f9457a9b3773622d0d
Sangmin Lee <sangminlee7648@gmail.com>
2021-03-28 18:04:18 +0900

    ADD: field dump data

---
+++ data_dumps/fields_dump.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
+--
+-- Host: localhost    Database: mhriseDB
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.18-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `fields`
+--
+
+LOCK TABLES `fields` WRITE;
+/*!40000 ALTER TABLE `fields` DISABLE KEYS */;
+INSERT INTO `fields` VALUES (1,'용암 동굴','lava_caverns','거대한 암석 동굴 속은 용암이 흐르는 뜨거운 구역과 아름답고 맑은 물이 흐르는 구역으로 나누어져 있다. 몬스터만 없다면 창홍이 반짝이는 신비로운 광경이지만...'),(2,'모래 평원','sandy_plains','혈육에 굶주린 몬스터들이 이빨을 드러내는 불모의 벌판. 햇볕이 사정없이 내리쬐는 낮과 달빛이 비치는 밤은 분위기가 아주 다르다.'),(3,'한랭 군도','frost_islands','극한의 한랭 군도는 한때 거대한 용이 소굴로 삼았던 곳. 수많은 강자가 그 용에 도전한 후 사라졌다는 죽음의 장소이다. 섬 한가운데에는 그 격렬한 전투를 말해주는 거대한 용뼈와 썩어버린 격룡선이 눈바람에 드러나 있다.'),(4,'수몰된 숲','flooded_forest','울창한 나무가 우거진 심록의 비경. 항상 물줄기가 끊기는 일이 없으며, 대부분이 물에 잠겨 있다. 한눈에도 눈길을 끄는 거대한 유적은 밧줄벌레를 사용하면 등정할 수 있을 것 같다.'),(5,'사원 폐허','shrine_ruins','진호의 기도를 드리는 사원이 있던 자리도 지금은 썩어 문드러져 낮밤을 가리지 않고 몬스터의 포효가 울려 퍼진다. 그러나 성스러운 장소의 모습은 아직 짙게 남아 있어, 신비로운 분위기에 싸여있다.');
+/*!40000 ALTER TABLE `fields` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-26 17:24:28
